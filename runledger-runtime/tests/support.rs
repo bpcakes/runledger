05d9135878555f0edf9d583d0a1ee40e819b8f27
featherenvy <96315340+featherenvy@users.noreply.github.com>
2026-08-08 09:14:30 +0200

    refactor: replace mod.rs module roots

--- runledger-runtime/tests/support.rs
+++ runledger-runtime/tests/support.rs
@@ -1,5 +1,7 @@
 pub use self::job_definition::register_job_definition;
 pub use self::query_error::query_error_code;
 
+#[path = "support/job_definition.rs"]
 mod job_definition;
+#[path = "support/query_error.rs"]
 mod query_error;
